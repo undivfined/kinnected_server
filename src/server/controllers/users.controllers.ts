64a9cf61ccc2ce5707c0486d1f74246cc2036498
marmaryia <marozavamaryia@gmail.com>
2025-04-15 12:29:42 +0100

    add get /api/users which responds with an array of all users if no query specified and accepts a search query consisting of only letters and representing only first name, only last name, or first and last names combined

---
+++ src/server/controllers/users.controllers.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+import { fetchUsers } from "../models/users.models";
+
+export function getUsers(request: Request, response: Response) {
+  const { search } = request.query;
+
+  fetchUsers(search).then((users) => {
+    response.status(200).send({ users });
+  });
+}
