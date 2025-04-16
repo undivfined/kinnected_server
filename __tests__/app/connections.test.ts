ddc8c11d538d96e0799f428931012058df0995d6
marmaryia <marozavamaryia@gmail.com>
2025-04-16 18:55:50 +0100

    update connections test to timezone-agnostic

--- __tests__/app/connections.test.ts
+++ __tests__/app/connections.test.ts
@@ -154,7 +154,7 @@ describe("/api/connections", () => {
               username_1: "amaraj_93",
               username_2: "liamc2020",
               type_of_relationship: "Partner",
-              date_of_last_contact: "2025-04-01T11:45:00.000Z",
+              date_of_last_contact: expect.any(String),
               messaging_link: "",
             },
           });
