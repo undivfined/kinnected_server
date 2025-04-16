b7b7b4a13409ebd0e8d7cc390f3fedc1780c4a11
marmaryia <marozavamaryia@gmail.com>
2025-04-16 15:36:32 +0100

    build for cors

--- dist/server/app.js
+++ dist/server/app.js
@@ -4,10 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const users_controllers_1 = require("./controllers/users.controllers");
 const connections_controllers_1 = require("./controllers/connections.controllers");
 const errors_controllers_1 = require("./controllers/errors.controllers");
 const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.get("/api/users", users_controllers_1.getUsers);
 app.post("/api/users", users_controllers_1.postUser);
